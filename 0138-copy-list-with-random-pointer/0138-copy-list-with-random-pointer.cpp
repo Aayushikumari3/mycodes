80078e0180797bbf35ee8972821b26ffece69930
Aayushi Kumari <127132101+Aayushikumari3@users.noreply.github.com>
2024-07-10 02:24:17 +0530

    Time: 10 ms (23.43%), Space: 12.8 MB (88.49%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+        while(!head)
+            return nullptr;
+         
+        // Step 1: Create new nodes and insert them after each original node
+        Node* curr = head;
+        while (curr) {
+            Node* newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr = newNode->next;
+        }
+        
+        // Step 2: Assign random pointers for the new nodes
+        curr = head;
+        while (curr) {
+            if (curr->random) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        
+        // Step 3: Separate the two lists
+        curr = head;
+        Node* newHead = head->next;
+        Node* copyCurr = newHead;
+        
+        while (curr) {
+            curr->next = curr->next->next;
+            if (copyCurr->next) {
+                copyCurr->next = copyCurr->next->next;
+            }
+            curr = curr->next;
+            copyCurr = copyCurr->next;
+        }
+        
+        return newHead;
+    }
+};
