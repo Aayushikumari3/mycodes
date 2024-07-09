401c1faae267217c4a85d1a459b729dc107e684a
Aayushi Kumari <127132101+Aayushikumari3@users.noreply.github.com>
2024-07-09 22:55:47 +0530

    Time: 0 ms (100.00%), Space: 15.1 MB (94.36%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,46 +8,79 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <deque>
+// #include <deque>
 
+// class Solution {
+// public:
+//     ListNode* rotateRight(ListNode* head, int k) {
+//         if (head == nullptr || k == 0) return head;
+
+//         std::deque<int> q;
+//         ListNode* temp = head;
+//         int len = 0;
+
+//         // Push all the values of the list into the deque
+//         while (temp != nullptr) {
+//             q.push_back(temp->val);
+//             temp = temp->next;
+//             len++;
+//         }
+
+//         // Reduce k to a manageable number
+//         k = k % len;
+//         if (k == 0) return head;
+
+//         // Rotate the deque k times to the right
+//         for (int i = 0; i < k; i++) {
+//             int g = q.back();
+//             q.pop_back();
+//             q.push_front(g);
+//         }
+
+//         // Create a new list from the deque
+//         ListNode* headn = new ListNode(q.front());
+//         q.pop_front();
+//         ListNode* newHead = headn;
+
+//         while (!q.empty()) {
+//             headn->next = new ListNode(q.front());
+//             q.pop_front();
+//             headn = headn->next;
+//         }
+
+//         return newHead;
+//     }
+// };
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (head == nullptr || k == 0) return head;
-
-        std::deque<int> q;
-        ListNode* temp = head;
-        int len = 0;
-
-        // Push all the values of the list into the deque
-        while (temp != nullptr) {
-            q.push_back(temp->val);
-            temp = temp->next;
+        // edge cases
+        if (!head || !head->next || k == 0) return head;
+        
+        // compute the length
+        ListNode *cur = head;
+        int len = 1;
+        while (cur->next) {
+            cur = cur->next;
             len++;
         }
-
-        // Reduce k to a manageable number
+        
+        // connect the end of the list to the head
+        cur->next = head;
+        
+        // compute the number of rotations needed
         k = k % len;
-        if (k == 0) return head;
-
-        // Rotate the deque k times to the right
-        for (int i = 0; i < k; i++) {
-            int g = q.back();
-            q.pop_back();
-            q.push_front(g);
+        k = len - k;
+        
+        // go to the new end of the list
+        while (k--) {
+            cur = cur->next;
         }
-
-        // Create a new list from the deque
-        ListNode* headn = new ListNode(q.front());
-        q.pop_front();
-        ListNode* newHead = headn;
-
-        while (!q.empty()) {
-            headn->next = new ListNode(q.front());
-            q.pop_front();
-            headn = headn->next;
-        }
-
-        return newHead;
+        
+        // make the node head and break the connection
+        head = cur->next;
+        cur->next = NULL;
+        
+        return head;
     }
 };
