c7ac8fbd9d788747b9b5bc9b41adfae7f5ff212d
Aayushi Kumari <127132101+Aayushikumari3@users.noreply.github.com>
2024-07-24 23:48:58 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (25.22%) - LeetHub

--- 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.cpp
+++ 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.cpp
@@ -3,14 +3,14 @@ public:
     int removeAlmostEqualCharacters(string word) 
     {
         int ans =0;
-        for(int i=1;i<word.size()-1;i++)
-        {
-            if(abs(word[i]-word[i-1])<=1 and abs(word[i]-word[i+1])<=1)
-            {
-                word[i]='#';
-                ans++;
-            }
-        }
+        // for(int i=1;i<word.size()-1;i++)
+        // {
+        //     if(abs(word[i]-word[i-1])<=1 and abs(word[i]-word[i+1])<=1)
+        //     {
+        //         word[i]='#';
+        //         ans++;
+        //     }
+        // }
         for(int i=1;i<word.size();i++)
         {
             if(abs(word[i]-word[i-1])<=1)
